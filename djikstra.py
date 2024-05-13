452eb372c7f43715e9ed3edd6fce236c5ab1b2ba
111Roainialfiona <148738023+Roainialfiona@users.noreply.github.com>
2024-05-13 21:18:10 +0700

    djikstra.py

--- djikstra.py
+++ djikstra.py
@@ -29,38 +29,35 @@ class Graph:
                     del self.cityList[kota][kotaDihapus]
             return True
         return False
+def tambahkanJalan(self, kota1, kota2, jarak):
+        if kota1 in self.cityList and kota2 in self.cityList:
+            self.cityList[kota1][kota2] = jarak
+            self.cityList[kota2][kota1] = jarak
+            return True
+        return False
 
-class Graph:
-    #initialization
-    def __init__(self):
-        self.cityList = {}
+    def hapusJalan(self, kota1, kota2):
+        if kota1 in self.cityList and kota2 in self.cityList:
+            if kota2 in self.cityList[kota1]:
+                del self.cityList[kota1][kota2]
+                del self.cityList[kota2][kota1]
+                return True
+        return False
 
-    def printGraph(self):
-        #mengiterasi setiap city
+    def djikstra(self, source):
+        
+        distances = {}
         for city in self.cityList:
-            #setiap kota print nama kota
-            print(city, ":", self.cityList[city])
-
-            # Print distances to neighboring cities
-            for neighbor, distance in self.cityList[city].items():
-                #print tetangga dan jarak
-                print("    ->", neighbor, ":", distance)
-
-    def tambahkanKota(self, kota):
-        #jika kota tidak ada di cityList
-        if kota not in self.cityList:
-            #maka tambahkan kota
-            self.cityList[kota] = {}
-            return True
-        return False
-    def hapusKota(self, kotaDihapus):
-        if kotaDihapus in self.cityList:
-            del self.cityList[kotaDihapus]
-            for kota in self.cityList:
-                if kotaDihapus in self.cityList[kota]:
-                    del self.cityList[kota][kotaDihapus]
-            return True
-        return False
+            distances[city] = float('inf')
+        
+        distances[source] = 0
+        print (distances)
+        # Initialize list of unvisited cities
+        unvisited_cities = []
+        for city in self.cityList:
+            unvisited_cities.append(city)
+        #unvisited_cities = list(self.cityList.keys())
+        print (unvisited_cities)
         
 while unvisited_cities:
             # Find the unvisited city with the smallest distance
